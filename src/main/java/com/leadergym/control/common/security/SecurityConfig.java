15cfb9e99558cd81502a1f7252a5d9d4dc6a608e
Joaquin Labrador <joaquinlabrador1010@gmail.com>
2026-02-18 16:46:20 +0100

    Feature: set spring security

---
+++ src/main/java/com/leadergym/control/common/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.leadergym.control.common.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/members/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
